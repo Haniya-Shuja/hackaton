468b92b966d65e4f7c472c66b0833e6a6f6c57cd
haniya shuja <haniyashuja786@gmailcom>
2024-12-09 00:41:30 +0500

    success

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,22 +12,27 @@ export const Header = ()=>{
     return(
     
 
-        <div className='flex w-[1170px] h-[38px] mb-4 ml-[135px]  mt-[88px] gap-[130px] '>
+        <div className='flex w-[1170px] h-[38px] mb-4 ml-[135px]  mt-[70px] gap-[130px] '>
         <h2 className="h-[24px] w-[118px] text-[24px] font-inter font-[700] text-[#000000] flex items-center">
           <Image src="/images/logo copy.png" alt="Logo" width={200} height={100} />
         </h2>
            <ul className='w-[370px] h-[24px] flex gap-[48px] text-[#000000]'>
-            <li className='font-poppins text-[16px] text-center font-normal w-[48px] h-[24px]'><Link href="/">Home</Link></li>
-            <li className='font-poppins text-[16px] text-center font-normal w-[66px] h-[24px]'><Link href="/shop" className="text-blue-500 hover:underline">Shop
+            <li className='font-poppins text-[16px] text-center font-normal w-[48px] h-[24px]'><Link href="/" className="text-black-500 hover:underline">Home</Link></li>
+            <li className='font-poppins text-[16px] text-center font-normal w-[66px] h-[24px]'><Link href="/shop" className="text-black-500 hover:underline">Shop
             </Link></li>
-            <li className='font-poppins text-[16px] text-center font-normal w-[48px] h-[24px]'><Link href="../about">About</Link></li>
-            <li className='font-poppins text-[16px] text-center font-normal w-[61px] h-[24px]'><Link href="/contact" className="text-blue-500 hover:underline">contact</Link></li>
+            <li className='font-poppins text-[16px] text-center font-normal w-[48px] h-[24px]'><Link href="../about" className="text-black-500 hover:underline">blog</Link></li>
+            <li className='font-poppins text-[16px] text-center font-normal w-[61px] h-[24px]'><Link href="/contact" className="text-black-500 hover:underline">contact</Link></li>
            </ul>
            <div className='flex gap-3'>
             <button className='flex border-2 border-[#F5F5F5]  pl-[20px] pt-[7px] pb-[7px] pr-[12px] bg-[#F5F5F5]  gap-[10px] rounded-[4px]'><input type="text" placeholder='What are you looking for?' className='w-[130pxx] h-[18px] text-[12px] font-poppins font-normal opacity-[50%] text-[#000000]'/><IoIosSearch className='w-[16px] h-[16px]'/></button>
-            <FaRegHeart className='h-[28px] w-[28px]'/>
-            <MdOutlineShoppingCart className='h-[28px] w-[28px]' />
             <FaRegUserCircle  className='h-[28px] w-[28px] '/>
+            <br/>
+            <br/>
+            <FaRegHeart className='h-[28px] w-[28px]'/>
+            <br/>
+            <br/>
+            <MdOutlineShoppingCart className='h-[28px] w-[28px]' />
+            
            </div>
         </div>
         
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -5,15 +5,15 @@ import Card from './Card';
 const Hero = () => {
   return (
 <div className="relative w-full h-[50rem] bg-cover bg-center" style={{ backgroundImage: "url('/images/hero copy.png')" }}>
-  <div className="absolute top-2/3 left-2/4 transform -translate-y-1/2 text-left bg-orange-300 p-8 rounded-lg "style={{ width: "600px", height: "400px" }}>
+  <div className="absolute top-2/3 left-2/4 transform -translate-y-1/2 text-left bg-[#FFF3E3] p-8 rounded-lg "style={{ width: "600px", height: "400px" }}>
     <h6 className="text-lg font-bold">New Arrival</h6>
-    <h3 className="text-4xl my-4">
+    <h3 className="text-4xl my-4 font-bold text-[#B88E2F]">
       Discover Our <br /> New Collection
     </h3>
-    <p className="text-base mb-6">
+    <p className="text-base mb-6 ">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque dolore
     </p>
-    <button className="bg-[#ff6f61] text-white py-3 px-6 text-lg rounded-md z-10">
+    <button className="bg-[#B88E2F] text-white py-3 px-6 text-lg rounded-md z-10">
       BUY NOW
     </button>
   </div>
@@ -78,7 +78,7 @@ export const CardDetails = () => {
    
     {
       id: 1,
-      img: "/images/pro1.jpg",
+      img: "/images/pro3.jpg",
       title: "Syltherine",
        rate: "stylish Cafe Chair",
        price: "Rp: 2500000",
@@ -88,62 +88,63 @@ export const CardDetails = () => {
     },
     {
       id: 2,
-      img: "/images/pro2.jpg",
-      title: "HAVIT HV-G92 Gamepad",
-      price: "$120 $160",
-      rate: "/images/star.png",
+      img: "/images/pro7.jpg",
+      title: "Leviosa",
+      rate: "stylish Cafe Chair",
+      price: "250,000",
       
     },
     {
       id: 3,
-      img: "/images/pro3.jpg",
-      title: "HAVIT HV-G92 Gamepad",
-      price: "$120 $160",
-      rate: "/images/star.png",
+      img: "/images/pro6.jpg",
+      title: "Lolito",
+      rate: "Luxury big sofa",
+      price: "7000,000",
      
     },
     {
       id: 4,
-      img: "/images/pro3.jpg",
-      title: "HAVIT HV-G92 Gamepad",
-      price: "$120 $160",
-      rate: "/images/star.png",
+      img: "/images/pro8.jpg",
+      title: "Respira",
+      rate: "Outdoor bar table sofa",
+      price: "500,000",
      
     },
 
     //new4
     {
       id: 5,
-      img: "/images/pro1.jpg",
-      title: "Syltherine",
-       rate: "stylish Cafe Chair",
-       price: "Rp: 2500000",
+      img: "/images/pro5.jpg",
+      title: "Grifo",
+       rate: "Night Lamp",
+       price: "Rp:5000,000",
       
 
       
     },
     {
       id: 6,
-      img: "/images/pro2.jpg",
-      title: "HAVIT HV-G92 Gamepad",
-      price: "$120 $160",
-      rate: "/images/star.png",
+      img: "/images/pro6.jpg",
+      title: "Muggo",
+      
+      rate: "small mug",
+      price: "Rp 150,000",
       
     },
     {
       id: 7,
-      img: "/images/pro3.jpg",
-      title: "HAVIT HV-G92 Gamepad",
-      price: "$120 $160",
-      rate: "/images/star.png",
+      img: "/images/pro8.jpg",
+      title: "Pingky",
+      rate: "cute bed set",
+      price: "RP:7000000",
      
     },
     {
       id: 8,
-      img: "/images/pro3.jpg",
-      title: "HAVIT HV-G92 Gamepad",
-      price: "$120 $160",
-      rate: "/images/star.png",
+      img: "/images/pro2.jpg",
+      title: "Potty",
+      rate: "minimalist flower pot",
+      price: "RP 500,000",
      
     },
    
@@ -211,61 +212,3 @@ export const Hero3 = () => {
   );
 };
 export default Hero;
-
-// export const Hero3 = () => {
-//   return (
-//     <div className="flex flex-col gap-6">
-//       <Image
-//         src="/images/HERO2.png"
-//         alt="hero"
-//         width={1000}
-//         height={1000}
-//         className="w-full md:w-[1050px] h-auto mx-auto rounded-lg"
-//       />
-
-//       <div className="flex items-center gap-4">
-//         <div className="w-5 h-10 bg-[#DB4444] rounded"></div>
-//         <h2 className="text-[#DB4444] font-semibold text-lg">Our Products</h2>
-//       </div>
-
-//       <div className="flex justify-between items-center">
-//         <h2 className="font-inter font-semibold text-4xl">Explore Our Products</h2>
-//         <div className="flex gap-2">
-//           <button className="bg-gray-200 p-4 rounded-full">
-//             <FaArrowLeft />
-//           </button>
-//           <button className="bg-gray-200 p-4 rounded-full">
-//             <FaArrowRight />
-//           </button>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Hero;
-
-
-//       <div className="flex justify-between items-center"> 
-//       <h2 className="font-inter font-semibold text-4xl">Flash Sales</h2>
-//         <Image
-//           src="/images/time.png"
-//           alt="time"
-//           width={1000}
-//           height={1000}
-//           className="w-[302px] h-auto"
-//         />
-//         <div className="flex gap-2">
-//           <button className="bg-gray-200 p-4 rounded-full">
-//             <FaArrowLeft />
-//           </button>
-//           <button className="bg-gray-200 p-4 rounded-full">
-//             <FaArrowRight />
-//           </button>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }; 
-
-
